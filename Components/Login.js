51e51430545154cbafce3211cfde9e3b7dee1fed
Logan Karnes <logan.karnes98@gmail.com>
2020-12-08 21:45:26 -0800

    setup home screen

--- Components/Login.js
+++ Components/Login.js
@@ -1,53 +1,70 @@
-import { auto } from 'async';
 import React, {useState} from 'react';
 import { StyleSheet, Text,TextInput, View, Button, TouchableOpacity } from 'react-native';
 const SignUp = () => {
+    var [form, setForm] = useState({})
+
+    const handleRegister = () => {
+        alert(form.first_name)
+    }
     return (
         <View style={style.form}>
                 <Text style={style.header}>Sign Up</Text>
                 <TextInput
                     style={style.input}
-                    placeholder='User Name'/>
+                    placeholder='User Name'
+                    onChangeText={text => setForm({...form, user_name:text})}
+                />
                 <TextInput
                     style={style.input}
-                    placeholder='First Name'/>
+                    placeholder='First Name'
+                    onChangeText={text => setForm({...form, first_name:text})}
+                />
                 <TextInput
                     style={style.input}
-                    placeholder='Last Name'/>
+                    placeholder='Last Name'
+                    onChangeText={text => setForm({...form, last_name:text})}
+                />
                 <TextInput
                     style={style.input}
-                    placeholder='Password'/>
+                    placeholder='Password'
+                    secureTextEntry={true}
+                    onChangeText={text => setForm({...form, password:text})}
+                />
                 <TextInput
                     style={style.input}
-                    placeholder='Confirm Password'/>
-                <TouchableOpacity style={style.confirm}><Text style={style.confirmText}>Create Account</Text></TouchableOpacity>
+                    placeholder='Confirm Password'
+                    secureTextEntry={true}
+                    onChangeText={text => setForm({...form, confirm_password:text})}
+                />
+                <TouchableOpacity style={style.confirm} onPress={handleRegister}><Text style={style.confirmText}>Create Account</Text></TouchableOpacity>
             </View>
     )
 }
 const SignIn = () => {
+    var [form, setForm] = useState({})
+    const handleSignIn = () => {
+        alert(form)
+    }
     return (
         <View style={style.form}>
                 <Text style={style.header}>Sign In</Text>
                 <TextInput
                     style={style.input}
-                    placeholder='User Name'/>
+                    placeholder='User Name'
+                    onChangeText={text => setForm({...form, user_name:text})}
+                />
                 <TextInput
                     style={style.input}
-                    placeholder='First Name'/>
-                <TextInput
-                    style={style.input}
-                    placeholder='Last Name'/>
-                <TextInput
-                    style={style.input}
-                    placeholder='Password'/>
-                <TextInput
-                    style={style.input}
-                    placeholder='Confirm Password'/>
-                <TouchableOpacity style={style.confirm}><Text style={style.confirmText}>Sign Into Account</Text></TouchableOpacity>
+                    placeholder='Password'
+                    secureTextEntry={true}
+                    onChangeText={text => setForm({...form, user_name:text})}
+                />
+                <TouchableOpacity style={style.confirm} onPress={handleSignIn}><Text style={style.confirmText}>Sign Into Account</Text></TouchableOpacity>
             </View>
     )
 }
 export default function Login(){
+    
     const [signUp, setSignUp] = useState(true)
     return (
         <View style={style.root}>

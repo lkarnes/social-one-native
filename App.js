5b4dc29d72510e8374ed0bc30a4bd22d4e01fa08
Logan Karnes <logan.karnes98@gmail.com>
2020-12-22 21:02:04 -0800

    setup redux;

--- App.js
+++ App.js
@@ -3,20 +3,29 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import {Provider} from 'react-redux';
+import {reducer} from './redux/reducer/index';
+import {createStore} from 'redux';
 import 'react-native-gesture-handler';
 
 import Home from './Screens/Home';
 import Feed from './Screens/Feed';
-
+const store = createStore(reducer);
 const Stack = createStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={Home} />
-        <Stack.Screen name='Feed' component={Feed} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false
+          }}>
+          <Stack.Screen name='Home' component={Home} />
+          <Stack.Screen name='Feed' component={Feed} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+    
   );
 }
 
---
+++ redux/actions/index.js
@@ -0,0 +1,8 @@
+
+
+export const signIn = (payload) => {
+    return {
+        payload: payload,
+        action: "SIGN_IN"
+    }
+}
